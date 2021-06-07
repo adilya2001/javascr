a468b6e47f99d77f0d68d5e235534c1c83467d1a
Adil <atrix_aaa@icloud.com>
2021-06-07 15:27:39 +0600

    part1.1

--- js/script.js
+++ js/script.js
@@ -10,6 +10,13 @@ genres : [],
 privat: false
 };
 
+const a = prompt('what was the last one?', ''),
+b = prompt('Whats the rait?',''),
+c = prompt('what was the last one?', ''),
+d = prompt('Whats the rait?','');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
+console.log(personalMovieDB);
 
