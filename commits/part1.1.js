e5aa1cfd98f8ddd17969bc2335392f960e4d6806
Adil <atrix_aaa@icloud.com>
2021-06-07 15:59:38 +0600

    part1.2

---
+++ commits/part1.1.js
@@ -0,0 +1,19 @@
+const numberOfFilms = prompt('How many films have you watched','');
+
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors:{},
+genres : [],
+privat: false
+};
+
+const a = prompt('what was the last one?', ''),
+b = prompt('Whats the rait?',''),
+c = prompt('what was the last one?', ''),
+d = prompt('Whats the rait?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
 "use strict";
 
-const numberOfFilms = prompt('How many films have you watched','');
+if(4 == 9){
+    console.log('ok');
+}
+else{
+    console.log('wrong');
+}
 
-const personalMovieDB = {
-count: numberOfFilms,
-movies: {},
-actors:{},
-genres : [],
-privat: false
-};
 
-const a = prompt('what was the last one?', ''),
-b = prompt('Whats the rait?',''),
-c = prompt('what was the last one?', ''),
-d = prompt('Whats the rait?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// if(num < 49){
+//     console.log('error');
+// }
+// else if(num > 100){
+//     console.log('lot');
+// }
+// else{
+//     console.log('ok');
+// }
 
-console.log(personalMovieDB);
-
+// (num == 50) ? console.log('ok') :  console.log('error');
+const num = 50;
+switch (num) {
+ case 49:
+     console.log('wrong');
+     break;
+     case 100:
+     console.log('wrong');
+     break;
+     case 50:
+     console.log('ok');
+     break;
+     default:
+        console.log('loose');
+        break;
+}
